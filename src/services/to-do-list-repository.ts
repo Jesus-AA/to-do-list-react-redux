78cb9f41faca7ff7d81ed2691e24a6e4d5cd05f1
Jesus Alvarez <jesusalvarezaguilar94@gmail.com>
2023-10-11 15:28:05 +0200

    Add methods to repository class

--- src/services/to-do-list-repository.ts
+++ src/services/to-do-list-repository.ts
@@ -1,8 +1,61 @@
+import { Task, TaskNoId } from '../model/task';
+
 export class ApiToDoListRepository {
   urlBase: string;
   constructor(urlBase: string) {
     this.urlBase = urlBase;
   }
 
-  async getAll() {}
+  async getAll(): Promise<Task[]> {
+    const response = await fetch(this.urlBase);
+    if (!response.ok)
+      throw new Error(
+        `Error ${response.status}: ${(await response).statusText}`
+      );
+    const toDoList = await response.json();
+    return toDoList;
+  }
+
+  async getById(id: string): Promise<Task> {
+    const url = this.urlBase + '/' + id;
+    const response = await fetch(url);
+    if (!response.ok)
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    const task = await response.json();
+    return task;
+  }
+
+  async create(item: TaskNoId): Promise<Task> {
+    const response = await fetch(this.urlBase, {
+      method: 'POST',
+      body: JSON.stringify(item),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (!response.ok)
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    const newTask = await response.json();
+    return newTask;
+  }
+
+  async update(id: string, item: Partial<Task>): Promise<Task> {
+    const url = this.urlBase + '/' + id;
+    const response = await fetch(url, {
+      method: 'PATCH',
+      body: JSON.stringify(item),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (!response.ok)
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    const updatedTask = await response.json();
+    return updatedTask;
+  }
+
+  async delete(id: string): Promise<void> {
+    const url = this.urlBase + '/' + id;
+    const response = await fetch(url, {
+      method: 'DELETE',
+    });
+    if (!response.ok)
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+  }
 }
